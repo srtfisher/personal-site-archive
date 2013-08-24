db0ad7dffd65373030716b407291ab8db8619e62
Sean Fisher <srtfisher@gmail.com>
2013-08-24 15:10:43 -0400

    FIXING GOOD STUFF

--- wp-content/themes/sf-2013/page-home.php
+++ wp-content/themes/sf-2013/page-home.php
@@ -6,6 +6,12 @@
 		WordPress, front and backend Development and custom application
 		development. I work with individuals and business to create amazing
 		internet applications. Let me help create your.</p>
+
+                <p class="action-btns">
+                    <a href="<?php echo home_url('about'); ?>" class="btn btn-default btn-lg pull-left">About Me</a>
+                    <a href="<?php echo home_url('services'); ?>" class="btn btn-primary btn-lg">What I Can Do For You</a>
+                    <a href="<?php echo home_url('services'); ?>" class="btn btn-default btn-lg pull-right">Contact Me</a>
+                </p>
 	</div>
 </section>
 
@@ -84,102 +90,6 @@ Suspendisse a sapien nulla. Sed id iaculis lacus. Praesent scelerisque imperdiet
 	</div>
 </section>
 
-<section class="focus-area">
-	<h2 class="leading">What have I been up to?</h2>
-	
-	<div class="container">
-		<div class="row">
-			<div class="span2">&nbsp;</div>
-			<div class="span4">
-				Item
-
-			</div>
-
-			<div class="span4">
-				Item
-
-			</div>
-			<div class="span2">&nbsp;</div>
-
-		</div>
-	</div>
-</section>
-
-    <section class="below-spotlight">
-        <div class="inner">
-            <div class="container">
-                <p class="lead text">I'm a <strong>full stack developer</strong> from New York City. I love code, WordPress, GIT and <span class="red">you</span>.</p>
-            </div>
-        </div>
-    </section>
-    
-    <section class="services" id="about">
-        <div class="container">
-            <div class="row">
-                <div class="span3">
-                    <p class="lead">The world is becoming a smaller and smaller place. The rise of the Internet has allowed people across the globe to feel like they are right next to one another, even if it is virtually. This is amazing for those who haven't had a computer for most of their life.</p>
-                    <p class="lead">However, for those who have grown up with a computer and a smart phone, this is just the beginning. We will push technology further and further to new lengths. Instead of thinking what is impossible and what cannot be achieved, we think of what we want to see in the world.</p>
-                    <p class="lead">We are the change in the world, the well needed <span class="red">fire</span> in history books. We will make a dent in the universe &mdash; not next year, not ten years from now, today.</p>
-                    
-                    <hr />
-                    <p>Want Sean's paper-style resume? Visit my <a href="http://linkedin.com/in/srtfisher" target="_blank">LinkedIn</a> or <a href="http://seanfisher.co/resume" target="_blank">download it</a>
-                    
-                </div>
-                
-                <div class="span9">
-                    <div class="work-history clearfix">
-                        <div class="span5 col">
-                            <p>I work for as the CTO for Teens in Tech Labs. <strong>Teens in Tech Labs</strong> is a company that provides tools and resources to young entrepreneurs world-wide. We run an incubator to help give young entrepreneurs the skills they need to start their own company &mdash; as well as have them present at our annual conference. Throughout our online and offline initivites, we help connect young entrepreneurs connect around the world and receive the skills they need.</p>
-                        </div>
-                        <div class="span4 col right" style="">
-                            <a href="http://teensintech.com/"><img src="http://75f0.http.cdn.softlayer.net/0075F0/media/logo-300.jpg" /></a>
-                        </div>
-                        
-                        <!-- Clearing it out between -->
-                        <div class="clearfix"></div>
-                        <div class="spacer"></div>
-                        
-                        <div class="span4 col ">
-                            <a href="http://truanttoday.com/"><img src="http://s3.truanttoday.com/2012/04/tt-email-logo.png" /></a>
-                        </div>
-                        <div class="span5 col right">
-                            <p><strong>TruantToday</strong> works to keep parents virtually in the classroom by alerting them about their student. As the CTO, I work to build the frontend and backend infrastructure to make sure the experience is seamless and to help empower parents and guardians around the world.</p>
-                        </div>
-                        
-                    </div>
-                    
-                    <div class="freelance-cta">
-                        <span>I still accept freelancing requests! Let's <a href="#contact">get in touch.</a></span>
-                    </div>
-                    
-                    <div class="quotes">
-                        <div class="quote clearfix">
-                            <a href="http://twitter.com/danielbru" class="left span2" rel="nofollow"><img src="http://a0.twimg.com/profile_images/2503703736/411390_472724532745507_1521460306_o.jpg" class="img-circle" /></a>
-                            
-                            <p>"Love having Sean Fisher in the Teens in Tech Labs office! So many great things are getting done right now."</p>
-                            <cite>Daniel Brusilovsky, CEO and Founder Teens in Tech Labs</cite>
-                        </div>
-                        
-                        <div class="quote clearfix">
-                            <a href="http://twitter.com/kukoff" class="left span2" rel="nofollow"><img src="http://s3.truanttoday.com/2012/02/zak.jpg" class="img-circle" /></a>
-                            
-                            <p>"You are the Beethoven of code."</p>
-                            <cite>Zak Kukoff, CEO and Founder of TruantToday</cite>
-                        </div>
-                        <!--
-                        <div class="quote clearfix">
-                            <a href="http://twitter.com/kukoff" class="left span2" rel="nofollow"><img src="http://s3.truanttoday.com/2012/02/zak.jpg" class="img-circle" /></a>
-                            
-                            <p>"You are the Beethoven of code."</p>
-                            <cite>Zak Kukoff, CEO and Founder of TruantToday</cite>
-                        </div>
-                        -->
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
     
     <section class="contact" id="contact">
         <div class="container">
