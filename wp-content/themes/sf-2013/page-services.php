c36f9d738eef16e4f7fe6c91e65b2e5ab5a73e1d
Sean Fisher <srtfisher@gmail.com>
2013-08-30 11:51:17 -0400

    Services

---
+++ wp-content/themes/sf-2013/page-services.php
@@ -0,0 +1,71 @@
+<?php get_header(); ?>
+<section class="focus-area services">
+	<div class="container">
+		<h2 class="leading">Are you doing the <span class="yellow">best</span> for your business?</h2>
+		<p class="leading">Complete listing can be found on <a href="http://github.com/srtfisher" target="_blank" rel="self">GitHub</a></p>
+	</div>
+</section>
+
+<section class="solutions-process">
+	<div class="container">
+		<h2 class="leading">A process designed for you.</h2>
+		<p class="leading">Working around your needs, we ensure maximum happiness.</p>
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-sm-4 solution-item">
+				<img src="http://placehold.it/150x150" class="img-circle" />
+
+				<h4>Cum sociis natoque penatibus et magnis dis parturient montes.</h4>
+				<p>Aenean egestas nisi ut odio mollis elementum. Curabitur vulputate felis sit amet quam vulputate, vel laoreet orci accumsan.</p>
+			</div>
+
+			<div class="col-lg-4 col-md-4 col-sm-4 solution-item">
+				<img src="http://placehold.it/150x150" class="img-circle" />
+
+				<h4>Cum sociis natoque penatibus et magnis dis parturient montes.</h4>
+				<p>Aenean egestas nisi ut odio mollis elementum. Curabitur vulputate felis sit amet quam vulputate, vel laoreet orci accumsan.</p>
+			</div>
+
+			<div class="col-lg-4 col-md-4 col-sm-4 solution-item">
+				<img src="http://placehold.it/150x150" class="img-circle" />
+
+				<h4>Cum sociis natoque penatibus et magnis dis parturient montes.</h4>
+				<p>Aenean egestas nisi ut odio mollis elementum. Curabitur vulputate felis sit amet quam vulputate, vel laoreet orci accumsan.</p>
+			</div>
+		</div>		
+	</div>
+</section>
+
+<section class="solutions-feature">
+	<div class="container">
+		<div class="row">
+			
+		</div>		
+	</div>
+</div>
+
+<section class="solutions-feature center-feature">
+	<div class="container">
+		<div class="row">
+
+		</div>		
+	</div>
+</div>
+
+<section class="solutions-feature right-feature">
+	<div class="container">
+		<div class="row">
+			
+		</div>		
+	</div>
+</div>
+
+<section class="solutions-feature cta-feature">
+	<div class="container">
+		<div class="row">
+			
+		</div>		
+	</div>
+</div>
+
+
+<?php get_footer(); ?>
