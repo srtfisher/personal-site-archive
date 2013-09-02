017f8f0c89e3271629edd0852f8d155794cc13f3
Sean Fisher <srtfisher@gmail.com>
2013-09-02 17:38:42 -0400

    better deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 ssh web@seanfisher.co << ENDSSH
-cd ~/sf;
+cd ~/sf
 git pull
+php on-deploy.php
 
 # Clear cache
 cd wordpress/;
 wp w3-total-cache flush
 
 # Force rebuild of CSS
-cd ../;
+cd ../
 rm -rf wp-content/themes/sf-2013/css/site.css
 php index.php >> /dev/null
 
